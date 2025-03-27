44a564d08068805464b4dd1661b5794278ad2490
fritzcrone <fritz.crone@gmail.com>
2025-03-27 16:31:05 +0100

    stop als variable?

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 Skript für die Neuseelandreise
 */
 // Karte initialiesieren
-let lat = -45.874167;
-let lng = 170.503611;
-let zoom = 13;
-
 let stop = {
     nr: 23,
     title: "Dunedin",
@@ -15,7 +11,7 @@ let stop = {
     zoom: 13
 };
 
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -24,14 +20,14 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
 }).addTo(map);
 
 // Marker zeichnen
-let marker = L.marker([lat, lng]).addTo(map);
+let marker = L.marker([stop.lat, stop.lng]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup(`
     <h2>Dunedin</h2>
     <ul>
-        <li>Geogr. Breite: ${lat.toFixed(3)}°</li>
-        <li>Geogr. Länge: ${lng.toFixed(3)}°</li>
+        <li>Geogr. Breite: ${stop.lat.toFixed(3)}°</li>
+        <li>Geogr. Länge: ${stop.lng.toFixed(3)}°</li>
     </ul>
 `).openPopup();
 
