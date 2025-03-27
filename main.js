c0cad264f35a2fce97ef17acf178893501e6aba7
fritzcrone <fritz.crone@gmail.com>
2025-03-27 15:49:50 +0100

    drei var für lat, lng,lat,zoom

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 Skript für die Neuseelandreise
 */
 // Karte initialiesieren
-let map = L.map('map').setView([-45.874167, 170.503611], 13);
+let lat = -45.874167;
+let lng = 170.503611;
+let zoom = 13;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,7 +15,7 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
 }).addTo(map);
 
 // Marker zeichnen
-let marker = L.marker([-45.874167, 170.503611]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
