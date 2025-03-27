5fbd6f1b3f86379d44db92cacf60b7e3f559b8e1
fritzcrone <fritz.crone@gmail.com>
2025-03-27 15:36:54 +0100

    kommentare

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+/*
+Skript für die Neuseelandreise
+*/
+// Karte initialiesieren
 var map = L.map('map').setView([-45.874167, 170.503611], 13);
+
+// Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 maxZoom: 19,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Marker zeichnen
 var marker = L.marker([-45.874167, 170.503611]).addTo(map);
+
+// Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
